e57631cef28ff35d8fdf78158eb7c2a9703d4731
andykeefe <andrewrkeefe@gmail.com>
2024-12-05 15:59:14 -0600

    fixed typos with libraries

--- crypto/exp/lib.py
+++ crypto/exp/lib.py
@@ -1,12 +1,12 @@
 
 LIB_REGEX = {
-    'OpenSSL': r'<openssl\/|evp.h|[Oo]penssl|OPENSSL|[Oo]penSSL',
+    'OpenSSL': r'<openssl\/|evp\.h|[Oo]penssl|OPENSSL|[Oo]penSSL',
     'GnuTLS': r'gnutls',
-    'PyCryptodome': r'from Crypto.',
+    'PyCryptodome': r'from Crypto\.',
     'py-cryptography': r'from (cryptography.fernet|cryptography.hazmat)',
     'WolfSSL/WolfCrypt': r'wolfssl|WOLFSSL|WolfSSL|Wolfssl|wc_Init',
     'Go Crypto': r'crypto\/(aes|cipher|des|dsa|ecdsa|ed25519|elliptic|md5|rc4|rsa|sha)',
-    'Libsodium': r'[Ll]ibsodium|sodium.h|LIBSODIUM',
+    'Libsodium': r'[Ll]ibsodium|sodium\.h|LIBSODIUM',
     'NaCl': r'#include "crypto_(box|scalarmult|sign|secretbox|stream|auth|onetimeauth)\.h"',
     'Botan': r'botan\/|BOTAN_',
     'Bouncy Castle': r'[Bb]ouncycastle|BOUNCYCASTLE', ## to be improved
